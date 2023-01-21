33f68712f4b8c97f98aa788730a58da33bbd4130
Ramanan <ramananselvam300@gmail.com>
2023-01-21 16:52:58 +0530

    commit

---
+++ Dept/Dept/Data/Department.cs
@@ -0,0 +1,15 @@
+﻿
+namespace Dept.Data;
+
+public class Department
+{
+    public int detp_id { get; set; }
+
+
+    public int dept_name { get; set; }
+
+    public DateOnly date_ {get; set;}
+
+}
+
+
---
+++ Dept/Dept/Data/DeptView.cs
@@ -0,0 +1,65 @@
+﻿using MySql.Data;
+using System;
+
+
+namespace Sept.Data;
+public class DeptView
+{
+    MySql.Data.MySqlClient.MySqlConnection conn;
+    string myConnectionString;
+    public DeptView()
+    {
+        this.myConnectionString = "server=localhost;uid=root;" +
+    "pwd=10032004@Ra;database=project01";
+
+        try
+        {
+            this.conn = new MySql.Data.MySqlClient.MySqlConnection();
+            this.conn.ConnectionString = myConnectionString;
+            this.conn.Open();
+        }
+        catch (MySql.Data.MySqlClient.MySqlException ex)
+        {
+            System.Console.Write("Ramanan");
+        }
+    }
+
+
+    public void Insert(string name_)
+    {
+        string name_p = $"CALL insert_(name_);";
+        MySql.Data.MySqlClient.MySqlCommand command = new MySql.Data.MySqlClient.MySqlCommand(name_p,conn);
+        command.ExecuteReader();
+       
+
+    }
+
+
+    public void Delete_(string name_)
+    {
+        string name_p = $"CALL delete_(name_);";
+        MySql.Data.MySqlClient.MySqlCommand command = new MySql.Data.MySqlClient.MySqlCommand(name_p, conn);
+        command.ExecuteReader();
+
+    }
+
+    public MySql.Data.MySqlClient.MySqlDataReader  Select(int id)
+    {
+        string name_p = $"CALL find_name(id);";
+        MySql.Data.MySqlClient.MySqlCommand command = new MySql.Data.MySqlClient.MySqlCommand(name_p, conn);
+        MySql.Data.MySqlClient.MySqlDataReader dt = command.ExecuteReader();
+        return dt; 
+    }
+
+    public int closeconnection()
+    {
+        conn.Close();
+        return 1;
+
+    }
+
+    
+
+
+
+}
